219ede6b023c1616934470c8267580e35b2b0589
BenIsRong <benisrong@gmail.com>
2024-12-19 20:27:34 +0800

    feat: check if role already exists

--- main.py
+++ main.py
@@ -22,8 +22,11 @@ async def create_role(ctx, *role_name:str):
     guild = ctx.guild
     role_name = " ".join(role_name)
     try:
-        await guild.create_role(name=role_name)
-        await ctx.send(f"role `{role_name}` created!")
+        if not role_name in [role.name for role in guild.roles]:
+            await guild.create_role(name=role_name)
+            await ctx.send(f"role `{role_name}` created!")
+        else:
+            await ctx.send(f"role `{role_name}` already exists!")
     except:
         await ctx.send(f"unable to create `{role_name}` :c")
 
